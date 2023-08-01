b5bc9325b25a91c085bf52b9df605aebb56e8963
ToriKnix <toriknixmusic@gmail.com>
2023-08-01 12:12:22 -0600

    my last changes before i turn this in. For some reason, servers are really hard for me to get a hold of

--- NewWeek14/db/index.js
+++ NewWeek14/db/index.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
-const sequelize = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASSWORD, {
-  host: config.DB_HOST,
-  dialect: config.DB_DIALECT,
+const sequelize = new Sequelize(config.DB_NAME, config.user, config.password, {
+  host: config.host,
+  dialect: config.dialect,
   logging: false 
 });
 
